80be6848efa3bb708d5020e938220b37b9af0b9c
salgue441 <carlossalguero441@gmail.com>
2022-12-28 20:04:34 -0600

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 
 // From theme
 import GlobalStyle from "./Theme/GlobalTheme"
